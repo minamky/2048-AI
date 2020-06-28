16a934e8ab5d28871e839bfcc61294fef1d84028
Cedric Ong <cedricong99@gmail.com>
2020-06-29 03:36:03 +0800

    modified AI and added AI to game code

--- pygame2048.py
+++ pygame2048.py
@@ -318,7 +318,113 @@ def main():
                     print(score)
 
 
-if __name__ == '__main__':
-    main()
+import evaluation_functions
 
+
+class AI:
+    def __init__(self):
+        self.monotonicity_weight = 50
+        self.empty_weight = 400
+        self.merges_weight = 500
+        self.total_weight = 100
+
+    def evaluate_board(self, grid_):
+        utility = 0
+        monotonicity = evaluation_functions.monotonicity(grid_)
+        empty_merges_total = evaluation_functions.total_empty_and_merges(grid_)
+        utility += monotonicity * self.monotonicity_weight
+        utility += empty_merges_total[0] * self.empty_weight
+        utility += empty_merges_total[1] * self.merges_weight
+        utility += empty_merges_total[2] * self.total_weight
+        return utility
+
+    def maximise(self, grid_, depth=0):
+        utilities = []
+        grids = [left(grid_), right(grid_), up(grid_), down(grid_)]
+        for _ in grids:
+            utilities.append(self.chance(_, depth + 1))
+        if depth == 0:
+            return utilities
+        else:
+            return max(utilities)
+
+    def chance(self, grid_, depth):
+        average_utility = 0
+        probabilities = evaluation_functions.probabilities(grid_)
+        if probabilities:
+            if depth >= 3:
+                for _ in probabilities:
+                    average_utility += self.evaluate_board(_[0]) * _[1]
+                return average_utility
+            for _ in probabilities:
+                average_utility += _[1] * self.maximise(_[0], depth + 1)
+            return average_utility
+        else:
+            return self.evaluate_board(grid_)
+
+    def get_best_move(self, grid_):
+        moves = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+        array_ = self.maximise(grid_)
+        if evaluation_functions.total_empty_and_merges(grid_)[0] == 0:
+            if grid_ == left(grid_):
+                array_[0] = 0
+            if grid_ == right(grid_):
+                array_[1] = 0
+            if grid_ == up(grid_):
+                array_[2] = 0
+            if grid_ == down(grid_):
+                array_[3] = 0
+        index = array_.index(max(array_))
+        return moves[index]
+
+
+ai = AI()
+
+
+def main_ai():
+    run = True
+    global grid
+    global tiles_
+    global score
+    clock.tick(15)
+    init_spawn_tiles(grid)
+    generate_tiles()
+    redraw_screen()
+    while run:
+        if check_game_over(grid):
+            print("GAME OVER")
+            return print(score)
+        move = ai.get_best_move(grid)
+        print(move)
+        if move == 'UP':
+            grid = up(grid, True)
+            spawn_tile(grid)
+            generate_tiles()
+            redraw_screen()
+            print(score)
+        elif move == 'DOWN':
+            grid = down(grid, True)
+            spawn_tile(grid)
+            generate_tiles()
+            redraw_screen()
+            print(score)
+        elif move == 'LEFT':
+            grid = left(grid, True)
+            spawn_tile(grid)
+            generate_tiles()
+            redraw_screen()
+            print(score)
+        elif move == 'RIGHT':
+            grid = right(grid, True)
+            spawn_tile(grid)
+            generate_tiles()
+            redraw_screen()
+            print(score)
+
+
+# if __name__ == '__main__':
+#     main()
+#     pygame.quit()
+
+main_ai()
 pygame.quit()
